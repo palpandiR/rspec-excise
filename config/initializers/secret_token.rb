c77abba8d625c076cdfb934c00e603e8f03d4125
railsfactory-palpandi <palpandi@railsfactory.org>
2014-01-06 11:13:51 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RspecExcise::Application.config.secret_token = 'a6ae3aceaf657d3148431c7ff2862b07da7c1676afeb04b7f2b69e13d4e84f1e503d350e20ae070417d98f26bcd76f006088eae6884fd43eab3469f541767dcc'
